a915555420a3bb411b43b3ec387b09378f664c65
renansanttos <renansantttos@gmail.com>
2023-12-25 01:02:54 -0300

    meu codigo

---
+++ index.js
@@ -0,0 +1,20 @@
+let nome = "Sapildo"
+let xp = 9999
+
+if (xp <= 1000){
+   console.log("O nome do heroi é: " + nome + " e está no nível Ferro")
+}else if(xp <= 2000){
+    console.log("O nome do heroi é: " + nome + " e está no nível Bronze")
+}else if (xp <= 5000){
+    console.log("O nome do heroi é: " + nome + " e está no nível Prata")
+}else if (xp <=7000){
+    console.log("O nome do heroi é: " + nome + " e está no nível Ouro")
+}else if (xp <= 8000){
+    console.log("O nome do heroi é: " + nome + " e está no nível Platina")
+}else if (xp <=9001){
+    console.log("O nome do heroi é: " + nome + " e está no nível Ascendente")
+}else if (xp <= 10000){
+    console.log("O nome do heroi é: " + nome + " e Você está no nível Grêmio, pois, apenas você e o Tricolor são IMORTAIS!")
+}else{
+    console.log("O nome do heroi é: " + nome + " e está no nível Radiante")
+}
